82234cd6ac2d6d0915ba8354a0993bc59ff5ae6a
Tomer-Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-09-27 01:11:14 +0000

    LazyLock::new(Box(..stuff)) here as well

--- symphonia-bundle-mp3/src/layer3/requantize.rs
+++ symphonia-bundle-mp3/src/layer3/requantize.rs
@@ -15,25 +15,25 @@ use super::{codebooks, common::*, GranuleChannel};
 use std::cmp::min;
 use std::{f32, f64};
 
-use lazy_static::lazy_static;
+use std::sync::LazyLock;
 
 use log::debug;
 
-lazy_static! {
-    /// Lookup table for computing x(i) = s(i)^(4/3) where s(i) is a decoded Huffman sample. The
-    /// value of s(i) is bound between 0..8207.
-    static ref REQUANTIZE_POW43: [f32; 8207] = {
-        // It is wasteful to initialize to 0.. however, Symphonia policy is to limit unsafe code to
-        // only symphonia-core.
-        //
-        // TODO: Implement generic lookup table initialization in the core library.
-        let mut pow43 = [0f32; 8207];
-        for (i, pow43) in pow43.iter_mut().enumerate() {
-            *pow43 = f32::powf(i as f32, 4.0 / 3.0);
-        }
-        pow43
-    };
-}
+
+/// Lookup table for computing x(i) = s(i)^(4/3) where s(i) is a decoded Huffman sample. The
+/// value of s(i) is bound between 0..8207.
+static REQUANTIZE_POW43: LazyLock<Box<[f32; 8207]>> = LazyLock::new(|| {
+    // It is wasteful to initialize to 0.. however, Symphonia policy is to limit unsafe code to
+    // only symphonia-core.
+    //
+    // TODO: Implement generic lookup table initialization in the core library.
+    let mut pow43 = [0f32; 8207];
+    for (i, pow43) in pow43.iter_mut().enumerate() {
+        *pow43 = f32::powf(i as f32, 4.0 / 3.0);
+    }
+    Box::new(pow43)
+});
+
 
 /// Zero a sample buffer.
 #[inline(always)]
@@ -61,7 +61,7 @@ pub(super) fn read_huffman_samples<B: ReadBitsLtr>(
     }
 
     // Dereference the POW43 table once per granule since there is a tiny overhead each time a
-    // lazy_static is dereferenced that should be amortized over as many samples as possible.
+    // this is dereferenced so that should be amortized over as many samples as possible.
     let pow43_table: &[f32; 8207] = &REQUANTIZE_POW43;
 
     let mut bits_read = 0;
